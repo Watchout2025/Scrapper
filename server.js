b003a9bff6b693ee3964ef61bb08a3f4e8f3db1b
Watchout2025 <watchoutofficial2006@gmail.com>
2026-03-22 23:41:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+  res.send("Scraper running");
+});
+
+app.get("/scrape", async (req,res)=>{
+
+  const url = req.query.url;
+
+  let stream = null;
+
+  const browser = await puppeteer.launch({
+    args:["--no-sandbox","--disable-setuid-sandbox"]
+  });
+
+  const page = await browser.newPage();
+
+  page.on("response", response=>{
+    const u = response.url();
+    if(u.includes(".m3u8")) stream = u;
+  });
+
+  await page.goto(url,{waitUntil:"networkidle2"});
+
+  await page.waitForTimeout(5000);
+
+  await browser.close();
+
+  res.json({m3u8:stream});
+});
+
+app.listen(3000,()=>{
+  console.log("Server running");
+});
